977fddae094bc6aec2c1655ce93d1c710bd90e33
domjos1994 <developing@domjos.de>
2024-11-29 12:24:31 +0100

    Add Chapter 3

---
+++ Chapter03/Test.java
@@ -0,0 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * This is the first app!
+ */
+public class Test {
+
+    /**
+     * This is the Main-Class
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+
+        // print to console
+        System.out.println("Arguments: " + String.join("," , args));
+
+
+        Date current = new Date();
+        var sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        System.out.println("Datum: " + sdf.format(current));
+    }
+}
